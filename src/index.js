9ca64a2411c0b255f70809737a1d0311c8b250b5
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-11-02 10:45:25 -0700

    Remove duplicate game
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import Phaser from 'phaser';
 import config from './config/config';
 import GameScene from './scenes/GameScene';
 
-const game = new Phaser.Game(config);
-
 class Game extends Phaser.Game {
     constructor() {
         super(config);
